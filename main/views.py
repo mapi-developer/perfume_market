d70b4d9e28e938b52a2852adee7732ec7051a310
Mapi <matvey2000000009@gmail.com>
2025-08-11 12:44:00 +0400

    Database integrarion

--- main/views.py
+++ main/views.py
@@ -1,12 +1,20 @@
 from django.http import request
 from django.shortcuts import render
 
+from goods.models import Categories
+
 def index(request: request):
-    return render(request, 'main/index.html')
+    categories = Categories.objects.all()
+
+    context = {
+       'categories': categories,
+   }
+
+    return render(request, 'main/index.html', context)
 
 def about(request: request):
    context = {
        'content': 'About Us',
-       'text_on_page': 'Best market exist.'
+       'text_on_page': 'Best market exist.',
    }
    return render(request, 'main/about.html', context)
