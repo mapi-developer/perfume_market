39c332b525fba44e24f3c90469b1aaf44c6a22a3
Mapi <matvey2000000009@gmail.com>
2025-08-12 12:51:11 +0400

    Paginator connected

--- goods/views.py
+++ goods/views.py
@@ -1,15 +1,23 @@
+from django.core.paginator import Paginator
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from goods.models import Products
 
 
-def catalog(request: request):
-    goods = Products.objects.all()
+def catalog(request: request, category_slug, page=1):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context = {
         "title": "Catalog",
-        "goods": goods,
+        "goods": current_page,
+        "slug_url": category_slug,
     }
     return render(request, "goods/catalog.html", context)
 
