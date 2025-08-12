0825f5461929701e08693ca13d66c3aaf457cf63
Mapi <matvey2000000009@gmail.com>
2025-08-12 14:45:31 +0400

    GET filters

--- goods/views.py
+++ goods/views.py
@@ -7,19 +7,27 @@ from goods.models import Products
 
 def catalog(request: request, category_slug):
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by:
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
-
     context = {
         "title": "Catalog",
         "goods": current_page,
         "slug_url": category_slug,
+        "on_sale": on_sale != None,
+        "order_by": order_by,
     }
     return render(request, "goods/catalog.html", context)
 
