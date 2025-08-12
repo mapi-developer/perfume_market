ee5c5d3dd4585f2dc525010484c4ff110a38763e
Mapi <matvey2000000009@gmail.com>
2025-08-12 14:20:39 +0400

    paginator done

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.core.paginator import Paginator
 
 from goods.models import Categories
 
@@ -7,3 +8,12 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag()
+def get_proper_elided_page_range(p, number, on_each_side=1, on_ends=1):
+    paginator = Paginator(p.object_list, p.per_page)
+    return paginator.get_elided_page_range(number=number, on_each_side=on_each_side, on_ends=on_ends)
+
+@register.filter()
+def is_int(value):
+    return isinstance(value, int)
