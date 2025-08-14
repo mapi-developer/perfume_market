e4f2b2d58c1f79dafc43197a92fa20c0902a571e
Mapi <matvey2000000009@gmail.com>
2025-08-14 21:41:16 +0400

    full text search

--- goods/utils.py
+++ goods/utils.py
@@ -1,10 +1,16 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from goods.models import Products
 
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
     
+    vector = SearchVector('name', 'description')
+    query = SearchQuery(query)
+
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by('-rank')
+    '''
     keywords = [word for word in query.split() if len(word) > 2]
 
     q_objects = Q()
@@ -14,3 +20,4 @@ def q_search(query):
         q_objects |= Q(name__icontains=token)
 
     return Products.objects.filter(q_objects)
+    '''
